82d822d728c78230a3d848c524350de3ec901a14
Vsevolod Tsiliurik <tsilvs@proton.me>
2025-09-07 19:08:29 +0200

    Inputs handled, working on outputs

--- src/har2oapi.ts
+++ src/har2oapi.ts
@@ -8,6 +8,11 @@ import type { Har } from "har-format"
 import { OpenAPIObject } from 'openapi3-ts'
 import { generateSpec } from 'har-to-openapi'
 import stripJsonComments from 'strip-json-comments'
+import pkgManif from '../package.json'
+import * as YAML from 'yaml'
+
+type PackageManifest = typeof pkgManif
+
 //import('strip-json-comments')
 
 /** 
@@ -17,17 +22,7 @@ import stripJsonComments from 'strip-json-comments'
  * + `type HarToOpenAPIConfig = import('har-to-openapi/dist/types').HarToOpenAPIConfig`
  * + `import type { HarToOpenAPIConfig } from 'har-to-openapi/dist/types'`
  */
-type HarToOpenAPIConfig_tmpComp = {
-	forceAllRequestsInSameSpec?: boolean,
-	addServersToPaths?: boolean,
-	guessAuthenticationHeaders?: boolean,
-	relaxedMethods?: boolean,
-	relaxedContentTypeJsonParse?: boolean,
-	filterStandardHeaders?: boolean,
-	logErrors?: boolean,
-	attemptToParameterizeUrl?: boolean,
-	dropPathsWithoutSuccessfulResponse?: boolean
-}
+type HarToOpenAPIConfig = Parameters<typeof generateSpec>[1]
 
 /**
  * App CLI options
@@ -38,7 +33,7 @@ type appConfig = {
 	out?: fs.PathLike
 }
 
-type HAR2OAPICLIParams = HarToOpenAPIConfig_tmpComp & appConfig
+type HAR2OAPICLIParams = HarToOpenAPIConfig & appConfig
 
 namespace HAR2OAPICLIParams {
 	/**
@@ -104,6 +99,74 @@ type ParamName = {
 */
 const pnf = (pns: ParamName[]) => (n: string): ParamName => pns.find(pn => pn.l == n)
 
+const printHelp = (usage: string) /* => (err: Error) */ => {
+	process.stdout.write(`${usage}\n`)
+	process.exit(0)
+}
+
+const printVer = (pkgManif: PackageManifest) /* => (err: Error) */ => {
+	process.stdout.write(`${pkgManif.name} ${pkgManif.version}\n`)
+	process.exit(0)
+}
+
+/**
+ * Loads a file.
+ * 
+ * File conditions:
+ * 
+ * | Condition | Necessary | Sufficient |
+ * |-----------|-----------|------------|
+ * | Exists    | Yes       | No         |
+ * | Filled    | Yes       | Yes        |
+ * 
+ * Therefore, in the same function we check for both, but throw a different error on different types of failures.
+ * 
+ * If no file or file invalid - prints error, exits by default.
+ * 
+ * //TODO: Maybe rewrite with workers for parallelism / multithreading?
+ */
+const loadFile = /* async */ (path: fs.PathLike) => (exitOnError: boolean = true): string => {
+	let stats: fs.Stats
+	try {
+		stats = fs.statSync(path)
+		if (stats.size > 0) {
+			return fs.readFileSync(path, `utf8`)
+		} else {
+			throw new Error(`File is empty.`)
+		}
+	} catch (err) {
+		erh()()(exitOnError)(err)
+	}
+}
+
+/** 
+ * Validate JSON file content.
+ * 
+ * When we want to validate:
+ * 
+ * 1. When we already have confirmed that a file exists
+ * 2. When we are loading the file - always
+ * 
+ * File loader function can be a validator function too?
+ * 
+ * It can:
+ * 
+ * 1. Load
+ * 	+ Store loaded data (curried clojure)? - no reason to do it here for now.
+ * 2. Validate by parsing - no other way to do it with JSON because it's a string without extra binary headers, unless relying on file name extension
+ * 	+ Validation already requires parsing, so it can return parsed data
+ * 	+ Can store parsed data (curried clojure)? - no reason to do it here for now.
+ */
+const jsonLoad = <T>(data: string): T => {
+	let datanc: string = ''
+	try {
+		datanc = stripJsonComments(data) // TODO: Decide if I need it all the time or not. It may come out as an overhead and slow down the runtime. But also gives more versatility.
+		return JSON.parse(datanc) as T
+	} catch (err) {
+		erh()()()(err)
+	}
+}
+
 /**
  * Parameter can be built from:
  * + default value
@@ -134,8 +197,17 @@ const pnf = (pns: ParamName[]) => (n: string): ParamName => pns.find(pn => pn.l 
 
 // OS
 
-const appinfo = {
-	name: `har2oapi`,
+type AppInfo = {
+	name: string,
+	configfile: string,
+	buildroot: string,
+	systemroot: string,
+	homeroot: string,
+	workdir: string
+}
+
+const ainf: AppInfo = {
+	name: `${pkgManif.name}`,
 	configfile: `config.jsonc`,
 	buildroot: `${__dirname}`,
 	systemroot: ``,
@@ -152,13 +224,13 @@ type SysPaths = {
 
 const paths: SysPaths = {
 	// 2. conf file @ app distro
-	builtin: path.normalize(`${appinfo.buildroot}/defaults/${appinfo.configfile}`),
+	builtin: path.normalize(`${ainf.buildroot}/defaults/${ainf.configfile}`),
 	// 3. conf file @ /etc/ dir
-	shared: path.normalize(`${appinfo.systemroot}/etc/${appinfo.name}/${appinfo.configfile}`),
+	shared: path.normalize(`${ainf.systemroot}/etc/${ainf.name}/${ainf.configfile}`),
 	// 4. conf file @ Home dir
-	home: path.normalize(`${appinfo.homeroot}/.config/${appinfo.name}/${appinfo.configfile}`),
+	home: path.normalize(`${ainf.homeroot}/.config/${ainf.name}/${ainf.configfile}`),
 	// 5. conf file @ Work dir
-	workdir: path.normalize(`${appinfo.workdir}/.${appinfo.name}.config.${appinfo.configfile}`)
+	workdir: path.normalize(`${ainf.workdir}/.${ainf.name}.config.${ainf.configfile}`)
 }
 
 /**
@@ -178,6 +250,15 @@ const defaults: HAR2OAPICLIParams = {
 	file: path.normalize(`./har.json`),
 	out: path.normalize(`./openapi.yaml`)
 }
+
+/*
+interface Config {
+	apiKey: string
+	// other properties
+}
+
+const config = require('./config.json') as Config
+*/
 
 // CLI
 
@@ -250,83 +331,9 @@ const usage: string = clu([
 	}
 ])
 
-const printHelp = (usage: string) /* => (err: Error) */ => {
-	process.stdout.write(usage)
-	process.exit(0)
-}
-
-/**
- * If (help) - Print Help, exit
- */
-
 options.help && printHelp(usage)
 
-/**
- * Take input file.
- * If (no file name) and (no STDIN pipe) - print error, exit
- */
-
-!options.file && process.stdin.isTTY && thr()(erh('Provide input via --file or stdin pipe.')()())
-
-/**
- * Loads a file.
- * 
- * File conditions:
- * 
- * | Condition | Necessary | Sufficient |
- * |-----------|-----------|------------|
- * | Exists    | Yes       | No         |
- * | Filled    | Yes       | Yes        |
- * 
- * Therefore, in the same function we check for both, but throw a different error on different types of failures.
- * 
- * If no file or file invalid - prints error, exits by default.
- * 
- * //TODO: Maybe rewrite with workers for parallelism / multithreading?
- */
-
-const loadFile = /* async */ (path: fs.PathLike) => (exitOnError: boolean = true): string => {
-	let stats: fs.Stats
-	try {
-		stats = fs.statSync(path)
-		if (stats.size > 0) {
-			return fs.readFileSync(path, `utf8`)
-		} else {
-			throw new Error(`File is empty.`)
-		}
-	} catch (err) {
-		erh()()(exitOnError)(err)
-	}
-}
-
-/** 
- * Validate JSON file content.
- * 
- * When we want to validate:
- * 
- * 1. When we already have confirmed that a file exists
- * 2. When we are loading the file - always
- * 
- * File loader function can be a validator function too?
- * 
- * It can:
- * 
- * 1. Load
- * 	+ Store loaded data (curried clojure)? - no reason to do it here for now.
- * 2. Validate by parsing - no other way to do it with JSON because it's a string without extra binary headers, unless relying on file name extension
- * 	+ Validation already requires parsing, so it can return parsed data
- * 	+ Can store parsed data (curried clojure)? - no reason to do it here for now.
- */
-
-const jsonLoad = (data: string): object => {
-	let datanc: string = ''
-	try {
-		datanc = stripJsonComments(data) // TODO: Decide if I need it all the time or not. It may come out as an overhead and slow down the runtime. But also gives more versatility.
-		return JSON.parse(datanc)
-	} catch (err) {
-		erh()()()(err)
-	}
-}
+options.version && printVer(pkgManif)
 
 /**
  * Loads configs from all possible config file locations, layering one over the other, all over hardcoded defaults.
@@ -347,10 +354,10 @@ const jsonLoad = (data: string): object => {
  * But it might be more scalable for future features than current strategy.
  * 
  * 
-*/
+ */
 const layerParams = (paths: SysPaths) => (defaults: HAR2OAPICLIParams): HAR2OAPICLIParams =>
 	Object.values(paths)
-		.map(path => jsonLoad(loadFile(path)(false)))
+		.map(path => jsonLoad<HAR2OAPICLIParams>(loadFile(path)(false)))
 		.reduce(HAR2OAPICLIParams.reducer, defaults)
 
 //const paramObj: HAR2OAPICLIParams = Object.values(paths).map(path => jsonLoad(loadFile(path)())).reduce(HAR2OAPICLIParams.reducer, defaults)
@@ -382,44 +389,58 @@ const runtimeParams: HAR2OAPICLIParams = {
  * 2. If there's an input file name - read from it
  *
  * // TODO:
-*
-* + Input
-* 	+ STDIN
-* 	+ File path -> Load
-* + Output
-* 	+ STDOUT
-* 	+ File path -> Write
-* 
-* // TODO: read HAR from somewhere <- Process file and stdin streams <- Detect and prioritize stdin stream over file; maybe fallback on using the only non-empty out of 2? Or throw error if there is an input file option while also stdin pipe?
-*/
-
-const { spec: openAPIObj }: HarToOpenAPISpec_tmpComp = await generateSpec(har, runtimeParams as HarToOpenAPIConfig_tmpComp)
+ *
+ * + Input
+ * 	+ STDIN
+ * 	+ File path -> Load
+ * + Output
+ * 	+ STDOUT
+ * 	+ File path -> Write
+ * 
+ * Reading HAR from inputs
+ * <- Process file and stdin streams
+ * <- Detect and prioritize stdin stream over file
+ * 	+ throw error if there is an input file option while HAS_PIPE?
+ * 	+ fallback on using the only non-empty out of 2?
+ */
 
 // process.stdin.on('data', chunk => input += chunk)
 // process.stdin.on('end', () => {
 // 	// process input here
 // })
 
-// // const serializers = {
-// // 	json: (obj: object) => JSON.stringify(obj),
-// // 	yaml: (obj: object) => YAML.stringify(obj)
-// // }
+// Runtime constants
 
-// const processInput = (): Har => {
-// 	// 1. Read a HAR input - file or pipe
-// 	// 2. Parse JSON
-// 	// 3. Return object. HAR Log structure is expected
-// 	// With the structure: har.log.entries[]
-// }
+const HAS_PIPE: boolean = !process.stdin.isTTY
+const HAS_FILE: boolean = !!options.file
+const HAS_INPUT: boolean = HAS_FILE || HAS_PIPE
 
-// const har: Har = processInput()
+/**
+ * Take input file.
+ * If (no file name) and (no STDIN pipe) - print error, exit
+ */
+!HAS_INPUT && thr()(erh('Provide input via --file or stdin pipe.')()())
 
-// const config: HarToOpenAPIConfig_tmpCompatSubset = params.config
+let har: Har = null
 
-// const openapi = await generateSpec(har, config)
-// const { spec, yamlSpec } = openapi
-// // spec = { ... } openapi spec schema document
-// // yamlSpec = string, "info: ..."
+if (HAS_FILE) {
+	har = jsonLoad<Har>(loadFile(options.file)(true))
+} else if (HAS_PIPE) {
+	let pipedHarData: string = ""
+	for await (const chunk of process.stdin) pipedHarData += chunk
+	har = jsonLoad<Har>(pipedHarData)
+}
+
+if (har === null) { thr()(erh('Provide input via --file or stdin pipe.')()()) }
+
+// Actually generating the specification
+
+const { spec: openAPIObj }: HarToOpenAPISpec_tmpComp = await generateSpec(har, runtimeParams as HarToOpenAPIConfig)
+
+// TODO: Serialize it into specified format
+
+//JSON.stringify(obj)
+//YAML.stringify(obj)
 
 // const processOutput = (handler, config): void => {
 // 	/*
@@ -430,4 +451,5 @@ const { spec: openAPIObj }: HarToOpenAPISpec_tmpComp = await generateSpec(har, r
 // 	*/
 // }
 
-// // return or write a YAML
+// TODO: Return serialized to specified output destination
+
